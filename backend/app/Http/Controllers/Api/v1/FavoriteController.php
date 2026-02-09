cebd03d88765f2561cd407b260b4c6e7b1edb845
dtutoff <denistutov@hotmail.com>
2026-02-09 14:25:57 +0300

    feat: add index method to FavoriteController for user favorites

--- backend/app/Http/Controllers/Api/v1/FavoriteController.php
+++ backend/app/Http/Controllers/Api/v1/FavoriteController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers\Api\v1;
 
+use App\Http\Resources\StopResource;
 use App\Models\Stop;
 use Illuminate\Http\JsonResponse;
 
 class FavoriteController extends Controller
 {
+
+    public function index()
+    {
+        $favorites = auth()->user()->favoriteStops()->with('transports')->get();
+
+        return StopResource::collection($favorites);
+    }
+
     public function toggle(Stop $stop): JsonResponse
     {
         $user = auth()->user();
