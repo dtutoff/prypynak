f27727f70a4c5eba4126b44192d27e45e72fcb60
dtutoff <denistutov@hotmail.com>
2026-02-09 11:43:12 +0300

    feat: add v1 prefix to api routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,10 +5,14 @@ use App\Http\Controllers\Api\v1\TransportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
-Route::apiResource('transports', TransportController::class)->only(['index', 'show']);
+Route::prefix('v1')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
 
-Route::apiResource('stops', StopController::class)->only(['index', 'show']);
+    Route::apiResource('transports', TransportController::class)->only(['index', 'show']);
+
+    Route::apiResource('stops', StopController::class)->only(['index', 'show']);
+});
+
