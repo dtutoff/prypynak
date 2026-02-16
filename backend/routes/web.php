8565f0d057609fbfc365f0e6e1d8689184ff1bda
dtutoff <denistutov@hotmail.com>
2026-02-16 16:17:54 +0300

    chore: remove default welcome view and set root API status route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['status' => 'API is running', 'version' => '1.0.0']);
 });
